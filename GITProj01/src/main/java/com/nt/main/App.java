f4f4e521c979980e87f751cea43292985e11f934
pranayak-dev <pranaya20122000@gmail.com>
2025-08-14 11:53:11 +0530

    initial commit

---
+++ GITProj01/src/main/java/com/nt/main/App.java
@@ -0,0 +1,14 @@
+package com.nt.main;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("Sum ::" + c);
+		
+	}
+
+}
